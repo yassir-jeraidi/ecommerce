51ad30726a76a25811c3b1636b7d236ba6b9c60b
yassir-jeraidi <yassir.junior.03@gmail.com>
2025-10-26 21:35:48 +0100

    feat: add discovery service (Eureka)

---
+++ discovery-service/src/test/java/com/enset/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enset.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
